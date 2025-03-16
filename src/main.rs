94456bbecdaed69dcf4de65fbb40ac6379fe2aec
WilliamRagstad <william.ragstad@gmail.com>
2025-03-16 22:29:06 +0100

    Improve logging

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub mod registers;
 pub mod vm;
 
 fn main() {
-    env_logger::init();
+    log_init();
     let input = std::env::args().nth(1).expect("No input file provided");
     let input = std::fs::read_to_string(input).expect("Failed to read input file");
     if let Some(program) = parse(&input) {
@@ -20,4 +20,10 @@ fn main() {
         let mut vm = VM::new(program);
         vm.execute(entrypoint);
     }
+}
+
+fn log_init() {
+    env_logger::Builder::from_default_env()
+        .format_timestamp(None)
+        .init();
 }
