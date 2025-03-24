be440e56c2222d6a3066a84246a0e111ff1d7137
William Rågstad <william.ragstad@gmail.com>
2025-03-24 14:02:24 +0100

    CLI with clap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,9 +1,19 @@
+use clap::Parser;
 use mips_vm::{parser::parse, vm::VM};
+
+/// Simple program to run MIPS VM
+#[derive(Parser)]
+#[command(name = "mips_cli")]
+#[command(about = "A simple MIPS VM CLI", long_about = None)]
+struct Cli {
+    /// Input file to run
+    input: String,
+}
 
 fn main() {
     log_init();
-    let input = std::env::args().nth(1).expect("No input file provided");
-    let input = std::fs::read_to_string(input).expect("Failed to read input file");
+    let args = Cli::parse();
+    let input = std::fs::read_to_string(args.input).expect("Failed to read input file");
     if let Some(program) = parse(&input) {
         let mut vm = VM::new(program);
         vm.execute(vm.entrypoint().expect("No entrypoint found"));
