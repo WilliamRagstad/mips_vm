e5126df6e44e9bccfa837198d1e671e6722cba4c
William Rågstad <william.ragstad@gmail.com>
2025-04-06 18:44:22 +0200

    Add compile and run to cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use mips_vm::{parser::parse, vm::VM};
 
 mod mmio;
@@ -8,34 +8,69 @@ mod mmio;
 #[command(name = "mips", bin_name = "mips")]
 #[command(about = "A simple MIPS VM", long_about = None)]
 struct Cli {
-    /// Input file to run
-    input: String,
-    /// Optional memory dump to file
-    #[arg(short, long)]
-    dump_file: Option<String>,
-    /// Do not compress memory dump
-    #[arg(short, long, default_value = "false")]
-    non_compressed: bool,
-    /// Shard size for compression
-    #[arg(short, long, default_value = "128")]
-    shard_size: usize,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Compile the input file
+    #[command(name = "compile", alias = "c")]
+    Compile {
+        /// Input file to compile
+        input: String,
+    },
+    /// Run the input file
+    #[command(name = "run", alias = "r")]
+    Run {
+        /// Input file to run
+        input: String,
+        /// Optional memory dump to file
+        #[arg(short, long)]
+        dump_file: Option<String>,
+        /// Do not compress memory dump
+        #[arg(short, long, default_value = "false")]
+        non_compressed: bool,
+        /// Shard size for compression
+        #[arg(short, long, default_value = "128")]
+        shard_size: usize,
+    },
 }
 
 fn main() {
     log_init();
     let args = Cli::parse();
-    let input = std::fs::read_to_string(args.input).expect("Failed to read input file");
-    if let Some(program) = parse(&input) {
-        let mmio = Vec::new();
-        let mut vm = VM::new(program, mmio);
 
-        if let Some(dump_file) = args.dump_file {
-            let dump = vm.memory().dump(!args.non_compressed, args.shard_size);
-            let dump_path = std::path::PathBuf::from(dump_file);
-            std::fs::write(&dump_path, dump).unwrap();
+    match args.command {
+        Commands::Compile { input } => {
+            let input_content = std::fs::read_to_string(input).expect("Failed to read input file");
+            if let Some(program) = parse(&input_content) {
+                println!("Compilation successful!");
+                // Add any additional compilation logic here
+            } else {
+                eprintln!("Failed to compile the input file.");
+            }
         }
+        Commands::Run {
+            input,
+            dump_file,
+            non_compressed,
+            shard_size,
+        } => {
+            let input_content = std::fs::read_to_string(input).expect("Failed to read input file");
+            if let Some(program) = parse(&input_content) {
+                let mmio = Vec::new();
+                let mut vm = VM::new(program, mmio);
 
-        vm.execute(vm.entrypoint().expect("No entrypoint found"));
+                if let Some(dump_file) = dump_file {
+                    let dump = vm.memory().dump(!non_compressed, shard_size);
+                    let dump_path = std::path::PathBuf::from(dump_file);
+                    std::fs::write(&dump_path, dump).unwrap();
+                }
+
+                vm.execute(vm.entrypoint().expect("No entrypoint found"));
+            }
+        }
     }
 }
 
